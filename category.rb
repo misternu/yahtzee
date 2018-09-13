1bace7eed794672e94244b8629523990f796b807
Timothy Meixell <misternu@gmail.com>
2018-09-12 20:10:34 -0500

    Add ones through sixes

--- category.rb
+++ category.rb
@@ -1,20 +1,55 @@
 # a score category in Yahtzee
 class Category
+  CATEGORY_TYPES = [
+    :yahtzee,
+    :ones,
+    :twos,
+    :threes,
+    :fours,
+    :fives,
+    :sixes
+  ]
   attr_reader :type, :score
   def initialize(type)
+    return false unless CATEGORY_TYPES.include?(type)
     @type = type
     @score = nil
   end
 
   def score_dice(dice)
     return false unless dice.length == 5
-    case type
-    when :yahtzee
-      @score = yahtzee?(dice) ? 50 : 0
-    end
+    self.send(type, dice)
   end
 
-  def yahtzee?(dice)
-    dice.map(&:value).uniq.count == 1
+  def yahtzee(dice)
+    @score = dice.map(&:value).uniq.count == 1 ? 50 : 0
+  end
+
+  def ones(dice)
+    number(dice, 1)
+  end
+
+  def twos(dice)
+    number(dice, 2)
+  end
+
+  def threes(dice)
+    number(dice, 3)
+  end
+
+  def fours(dice)
+    number(dice, 4)
+  end
+
+  def fives(dice)
+    number(dice, 5)
+  end
+
+  def sixes(dice)
+    number(dice, 6)
+  end
+
+  def number(dice, number)
+    @score = dice.count { |d| d.value == number } * number
   end
 end
--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -12,14 +12,32 @@ describe Category do
   describe '#score' do
     let(:dice) { Array.new(5) { Die.new(1) } }
     let(:bad_dice) { Array.new(4) { Die.new(1) } << Die.new(2) }
-    let(:category) { Category.new(:yahtzee) }
+    let(:category_yahtzee) { Category.new(:yahtzee) }
+    let(:category_ones) { Category.new(:ones) }
+    let(:category_twos) { Category.new(:twos) }
     it 'has a score of 50 if it is yahtzee and that is its type' do
-      category.score_dice(dice)
-      expect(category.score).to eq 50
+      category_yahtzee.score_dice(dice)
+      expect(category_yahtzee.score).to eq 50
     end
     it 'has a score of 0 if it is not yahtzee and should be' do
-      category.score_dice(bad_dice)
-      expect(category.score).to eq 0
+      category_yahtzee.score_dice(bad_dice)
+      expect(category_yahtzee.score).to eq 0
+    end
+    it 'works for ones with all ones' do
+      category_ones.score_dice(dice)
+      expect(category_ones.score).to eq 5
+    end
+    it 'works for ones with four ones' do
+      category_ones.score_dice(bad_dice)
+      expect(category_ones.score).to eq 4
+    end
+    it 'works for twos with no twos' do
+      category_twos.score_dice(dice)
+      expect(category_twos.score).to eq 0
+    end
+    it 'works for twos with one two' do
+      category_twos.score_dice(bad_dice)
+      expect(category_twos.score).to eq 2
     end
   end
 end
